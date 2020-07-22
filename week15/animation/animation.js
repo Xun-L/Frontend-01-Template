1eba4edb7e5c4dcaea866acc3f0e2880ef1c9671
LuckyXun <LuckyXun>
2020-07-23 02:05:48 +0800

    feat:animation

---
+++ week15/animation/animation.js
@@ -0,0 +1,154 @@
+export class Timeline {
+  constructor() {
+    this.animations = [];
+    this.requestId = null;
+    this.state = 'inited';
+  }
+  tick() {
+    let t = Date.now() - this.startTime;
+    let animations = this.animations.filter((n) => !n.isFinished);
+    for (let animation of animations) {
+      if (t < animation.delay) {
+        continue;
+      }
+
+      let {
+        object,
+        property,
+        template,
+        start,
+        end,
+        duration,
+        timingFunction,
+        delay,
+        startTime
+      } = animation;
+      let percent = timingFunction((t - delay - startTime) / duration);
+
+      if (t > duration + delay + startTime) {
+        percent = 1;
+        animation.isFinished = true;
+      }
+      let val = animation.valueFromProgression(percent);
+
+      object[property] = template(val);
+    }
+    if (animations.length) {
+      this.requestId = requestAnimationFrame(() => this.tick());
+    }
+  }
+  pause() {
+    if (this.state !== 'playing') {
+      return;
+    }
+    this.state = 'pause';
+    this.pauseTime = Date.now();
+    if (this.requestId !== null) {
+      cancelAnimationFrame(this.requestId);
+    }
+  }
+  resume() {
+    if (this.state !== 'pause') {
+      return;
+    }
+    this.state = 'playing';
+    this.startTime += Date.now() - this.pauseTime;
+    this.tick();
+  }
+
+  start() {
+    if (this.state !== 'inited') {
+      return;
+    }
+    this.state = 'playing';
+
+    this.startTime = Date.now();
+    this.tick();
+  }
+  restart() {
+    if (this.state === 'playing') {
+      this.pause();
+    }
+    this.startTime = Date.now();
+    this.animations = this.animations.map((n) => {
+      n.isFinished = false;
+      return n;
+    });
+    this.tick();
+  }
+  add(animation, startTime) {
+    this.animations.push(animation);
+    animation.isFinished = false;
+    if (this.state === 'playing') {
+      animation.startTime =
+        startTime !== undefined ? startTime : Date.now() - this.startTime;
+    } else {
+      animation.startTime = startTime !== undefined ? startTime : 0;
+    }
+  }
+}
+
+export class Animation {
+  constructor(
+    object,
+    property,
+
+    start,
+    end,
+    duration,
+    delay = 0,
+    timingFunction,
+    template
+  ) {
+    this.object = object;
+    this.template = template;
+    this.property = property;
+    this.start = start;
+    this.end = end;
+    this.duration = duration;
+    this.delay = delay;
+    this.timingFunction =
+      timingFunction ||
+      ((start, end) => {
+        return (t) => start + (t / duration) * (end - start);
+      });
+  }
+  valueFromProgression(progression) {
+    return this.start + progression * (this.end - this.start);
+  }
+}
+
+export class ColorAnimation {
+  constructor(
+    object,
+    property,
+
+    start,
+    end,
+    duration,
+    delay = 0,
+    timingFunction,
+    template
+  ) {
+    this.object = object;
+    this.template = template || ((v) => `rgba(${v.r},${v.g},${v.b},${v.a})`);
+    this.property = property;
+    this.start = start;
+    this.end = end;
+    this.duration = duration;
+    this.delay = delay;
+    this.timingFunction =
+      timingFunction ||
+      ((start, end) => {
+        return (t) => start + (t / duration) * (end - start);
+      });
+  }
+  valueFromProgression(progression) {
+    return {
+      r: this.start.r + progression * (this.end.r - this.start.r),
+      g: this.start.g + progression * (this.end.g - this.start.g),
+      b: this.start.b + progression * (this.end.b - this.start.b),
+      a: this.start.a + progression * (this.end.a - this.start.a)
+    };
+  }
+}
--- week15/main.js
+++ week15/main.js
@@ -1,110 +1,110 @@
-import { createElement } from './createElement.js';
-import {Carousel} from './carousel.vue'
+import { createElement } from './createElement.js'
+//import {Carousel} from './carousel.vue'
 
 console.log(Carousel)
-// class Carousel {
-//   constructor() {
-//     this.children = [];
-//   }
-//   setAttribute(name, val) {
-//     this[name] = val;
-//   }
-//   render() {
-//     let children = this.data.map((url) => {
-//       let element = <img src={url} />;
-//       element.addEventListener('dragstart', (event) => event.preventDefault());
-//       return element;
-//     });
+class Carousel {
+  constructor() {
+    this.children = []
+  }
+  setAttribute(name, val) {
+    this[name] = val
+  }
+  render() {
+    let children = this.data.map((url) => {
+      let element = <img src={url} />
+      element.addEventListener('dragstart', (event) => event.preventDefault())
+      return element
+    })
 
-//     let root = <div class="carousel">{children}</div>;
+    let root = <div class="carousel">{children}</div>
 
-//     let pos = 0;
-//     let nextPic = () => {
-//       let nextPos = (pos + 1) % this.data.length;
-//       let current = children[pos];
-//       let next = children[nextPos];
+    let pos = 0
+    let nextPic = () => {
+      let nextPos = (pos + 1) % this.data.length
+      let current = children[pos]
+      let next = children[nextPos]
 
-//       current.style.transition = 'ease 0s';
-//       next.style.transition = 'ease 0s';
-//       current.style.transform = `translateX(${-100 * pos}%)`;
-//       next.style.transform = `translateX(${100 - 100 * nextPos}%)`;
-//       setTimeout(() => {
-//         current.style.transition = '';
-//         next.style.transition = '';
-//         current.style.transform = `translateX(${-100 - 100 * pos}%)`;
-//         next.style.transform = `translateX(${-100 * nextPos}%)`;
-//         pos = nextPos;
-//       }, 16);
+      current.style.transition = 'ease 0s'
+      next.style.transition = 'ease 0s'
+      current.style.transform = `translateX(${-100 * pos}%)`
+      next.style.transform = `translateX(${100 - 100 * nextPos}%)`
+      setTimeout(() => {
+        current.style.transition = ''
+        next.style.transition = ''
+        current.style.transform = `translateX(${-100 - 100 * pos}%)`
+        next.style.transform = `translateX(${-100 * nextPos}%)`
+        pos = nextPos
+      }, 16)
 
-//       // setTimeout(nextPic, 3000);
-//     };
-//     nextPic();
-//     root.addEventListener('mousedown', (event) => {
-//       let startX = event.clientX;
+      // setTimeout(nextPic, 3000);
+    }
+    nextPic()
+    root.addEventListener('mousedown', (event) => {
+      let startX = event.clientX
 
-//       let lastPos = (pos - 1 + this.data.length) % this.data.length;
-//       let nextPos = (pos + 1) % this.data.length;
+      let lastPos = (pos - 1 + this.data.length) % this.data.length
+      let nextPos = (pos + 1) % this.data.length
 
-//       let current = children[pos];
-//       let last = children[lastPos];
-//       let next = children[nextPos];
+      let current = children[pos]
+      let last = children[lastPos]
+      let next = children[nextPos]
 
-//       current.style.transition = 'ease 0s';
-//       last.style.transition = 'ease 0s';
-//       next.style.transition = 'ease 0s';
+      current.style.transition = 'ease 0s'
+      last.style.transition = 'ease 0s'
+      next.style.transition = 'ease 0s'
 
-//       current.style.transform = `translateX(${-500 * pos}px)`;
-//       last.style.transform = `translateX(${-500 - 500 * lastPos}px)`;
-//       next.style.transform = `translateX(${500 - 500 * nextPos}px)`;
-//       let move = (event) => {
-//         let moveX = event.clientX - startX;
-//         current.style.transform = `translateX(${moveX - 500 * pos}px)`;
-//         last.style.transform = `translateX(${moveX - 500 - 500 * lastPos}px)`;
-//         next.style.transform = `translateX(${moveX + 500 - 500 * nextPos}px)`;
-//       };
-//       let up = (event) => {
-//         console.log('up');
-//         let offset = 0;
-//         if (event.clientX - startX > 250) {
-//           offset = 1;
-//         }
-//         if (event.clientX - startX < -250) {
-//           offset = -1;
-//         }
+      current.style.transform = `translateX(${-500 * pos}px)`
+      last.style.transform = `translateX(${-500 - 500 * lastPos}px)`
+      next.style.transform = `translateX(${500 - 500 * nextPos}px)`
+      let move = (event) => {
+        let moveX = event.clientX - startX
+        current.style.transform = `translateX(${moveX - 500 * pos}px)`
+        last.style.transform = `translateX(${moveX - 500 - 500 * lastPos}px)`
+        next.style.transform = `translateX(${moveX + 500 - 500 * nextPos}px)`
+      }
+      let up = (event) => {
+        console.log('up')
+        let offset = 0
+        if (event.clientX - startX > 250) {
+          offset = 1
+        }
+        if (event.clientX - startX < -250) {
+          offset = -1
+        }
 
-//         current.style.transition = '';
-//         last.style.transition = '';
-//         next.style.transition = '';
+        current.style.transition = ''
+        last.style.transition = ''
+        next.style.transition = ''
 
-//         current.style.transform = `translateX(${offset * 500 - 500 * pos}px)`;
-//         last.style.transform = `translateX(${
-//           offset * 500 - 500 - 500 * lastPos
-//         }px)`;
-//         next.style.transform = `translateX(${
-//           offset * 500 + 500 - 500 * nextPos
-//         }px)`;
+        current.style.transform = `translateX(${offset * 500 - 500 * pos}px)`
+        last.style.transform = `translateX(${
+          offset * 500 - 500 - 500 * lastPos
+        }px)`
+        next.style.transform = `translateX(${
+          offset * 500 + 500 - 500 * nextPos
+        }px)`
 
-//         pos = (pos - offset + this.data.length) % this.data.length;
+        pos = (pos - offset + this.data.length) % this.data.length
 
-//         document.removeEventListener('mouseup', up);
-//         document.removeEventListener('mousemove', move);
-//       };
-//       document.addEventListener('mousemove', move);
-//       document.addEventListener('mouseup', up);
-//     });
+        document.removeEventListener('mouseup', up)
+        document.removeEventListener('mousemove', move)
+      }
+      document.addEventListener('mousemove', move)
+      document.addEventListener('mouseup', up)
+    })
 
-//     return root;
-//   }
-//   mountTo(parent) {
-//     this.render().mountTo(parent);
-//   }
-// }
+    return root
+  }
+  mountTo(parent) {
+    this.render().mountTo(parent)
+  }
+}
 let data = [
   'https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg',
   'https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg',
   'https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg',
-  'https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg',
-];
-let component = <Carousel data={data}></Carousel>;
+  'https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg'
+]
+let component = <Carousel data={data}></Carousel>
 
-component.mountTo(document.body);
+component.mountTo(document.body)
